a31ff1325faf06f51e260931159ffd4ef97bb685
Lucas Lima T <lukaslymauchiha@gmail.com>
2019-06-18 16:32:59 -0300

    create NextNumberBigger

--- app/src/main/java/kata/academy/lln/MainActivity.kt
+++ app/src/main/java/kata/academy/lln/MainActivity.kt
@@ -2,11 +2,22 @@ package kata.academy.lln
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import kata.academy.lln.algorithms.NextNumberBigger
+import kata.academy.lln.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.buttonVerify.setOnClickListener {
+
+            val fragment = NextNumberBigger()
+
+        }
     }
 }
